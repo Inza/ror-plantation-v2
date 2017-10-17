8ae5e0db3f215eaf7210db304862757420bdb671
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-10-17 11:51:31 +0200

    Migrate DB

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,11 +2,16 @@
 #
 # Table name: answers
 #
-#  id         :integer          not null, primary key
-#  title      :string
-#  correct    :boolean          default(FALSE), not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  title       :string
+#  correct     :boolean          default(FALSE), not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  question_id :integer
+#
+# Indexes
+#
+#  index_answers_on_question_id  (question_id)
 #
 
 class Answer < ApplicationRecord
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017094419) do
+ActiveRecord::Schema.define(version: 20171017095044) do
 
   create_table "answers", force: :cascade do |t|
     t.string "title"
     t.boolean "correct", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "question_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "question_assignments", force: :cascade do |t|
