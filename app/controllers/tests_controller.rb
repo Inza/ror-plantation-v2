cc97097e96e722ac3916494e6a3f8491910127c2
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-10-17 10:30:43 +0200

    Remove support for JSON from tests controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,64 +1,41 @@
 class TestsController < ApplicationController
   before_action :set_test, only: [:show, :edit, :update, :destroy]
 
-  # GET /tests
-  # GET /tests.json
   def index
     @tests = Test.all
   end
 
-  # GET /tests/1
-  # GET /tests/1.json
   def show
   end
 
-  # GET /tests/new
   def new
     @test = Test.new
   end
 
-  # GET /tests/1/edit
   def edit
   end
 
-  # POST /tests
-  # POST /tests.json
   def create
     @test = Test.new(test_params)
 
-    respond_to do |format|
-      if @test.save
-        format.html { redirect_to @test, notice: 'Test was successfully created.' }
-        format.json { render :show, status: :created, location: @test }
-      else
-        format.html { render :new }
-        format.json { render json: @test.errors, status: :unprocessable_entity }
-      end
+    if @test.save
+      redirect_to @test, notice: 'Test was successfully created.'
+    else
+      render :new
     end
   end
 
-  # PATCH/PUT /tests/1
-  # PATCH/PUT /tests/1.json
   def update
-    respond_to do |format|
-      if @test.update(test_params)
-        format.html { redirect_to @test, notice: 'Test was successfully updated.' }
-        format.json { render :show, status: :ok, location: @test }
-      else
-        format.html { render :edit }
-        format.json { render json: @test.errors, status: :unprocessable_entity }
-      end
+    if @test.update(test_params)
+      redirect_to @test, notice: 'Test was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /tests/1
-  # DELETE /tests/1.json
   def destroy
     @test.destroy
-    respond_to do |format|
-      format.html { redirect_to tests_url, notice: 'Test was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to tests_url, notice: 'Test was successfully destroyed.'
   end
 
   private
