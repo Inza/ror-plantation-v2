eb37b81c8798831d13c2193bd351c0e3113007b3
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-10-24 10:22:45 +0200

    Make dashboard root controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'dashboard/index'
-
   resources :subjects
   resources :tests
 
-  root to: 'tests#index'
+  root to: 'dashboard#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
