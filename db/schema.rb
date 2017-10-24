13a00fe33cd18209696d58d0add3ba8d8baa423b
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-10-24 09:21:21 +0200

    Migrate DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017095044) do
+ActiveRecord::Schema.define(version: 20171024071747) do
 
   create_table "answers", force: :cascade do |t|
     t.string "title"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20171017095044) do
     t.text "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "difficulty"
     t.index ["topic_id"], name: "index_questions_on_topic_id"
   end
 
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -10,6 +10,7 @@
 #  code       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  difficulty :integer
 #
 # Indexes
 #
