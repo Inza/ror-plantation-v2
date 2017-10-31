53fbc50427a3d3c4bdb9bc3437ebd7da5958090b
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-10-31 14:38:11 +0100

    Migrate DB
    `rake db:migrate`

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: templates
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  subject_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_templates_on_subject_id  (subject_id)
+#
+
 class Template < ApplicationRecord
   belongs_to :subject
 
