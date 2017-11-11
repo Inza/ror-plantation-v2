1561bfdad92d5e594c1fefea40aa905f708d1c86
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-11-11 09:58:34 +0100

    Implement topics controller

--- app/controllers/subjects/topics_controller.rb
+++ app/controllers/subjects/topics_controller.rb
@@ -1,7 +1,46 @@
 class Subjects::TopicsController < ApplicationController
+  before_action :set_subject
+  before_action :set_topic, only: [:edit, :update, :destroy]
+
+  permits :title
+
   def new
+    @topic = Topic.new(subject: @subject)
   end
 
   def edit
+  end
+
+  def create(topic)
+    @topic = Topic.new(topic)
+    @topic.subject = @subject
+
+    if @topic.save
+      redirect_to subject_path(@subject), notice: 'Topic was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update(topic)
+    if @topic.update(topic)
+      redirect_to subject_path(@subject), notice: 'Topic was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @topic.destroy
+    redirect_to subject_path(@subject), notice: 'Topic was successfully destroyed.'
+  end
+
+private
+  def set_subject(id)
+    @subject = Subject.find(id)
+  end
+
+  def set_topic(topic_id)
+    @topic = Topic.find(topic_id)
   end
 end
