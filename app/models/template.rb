458967ae7f9cf5aa2d15305a1fd1783085531d04
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-11-28 10:10:54 +0100

    Use topics from template instead of subject in test generation

--- app/models/template.rb
+++ app/models/template.rb
@@ -37,9 +37,9 @@ class Template < ApplicationRecord
 private
   def add_questions_to_test(test)
     raise 'I cannot add questions to the test without subject' unless subject.present?
-    raise 'I cannot add questions to the test when subject does not have any topic' if subject.topics.empty?
+    raise 'I cannot add questions to the test when subject does not have any topic' if topics.empty?
 
-    subject.topics.each do |topic|
+    topics.each do |topic|
       question_id = topic.question_ids.to_a.sample
       question = Question.find(question_id)
 
