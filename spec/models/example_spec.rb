7767666fe55d59288d180466296cc82ef221f2c9
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-12-05 10:56:58 +0100

    Implement example spec using factories

--- spec/models/example_spec.rb
+++ spec/models/example_spec.rb
@@ -18,5 +18,9 @@
 require 'rails_helper'
 
 RSpec.describe Example, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is created valid" do
+    example = build(:example)
+    example.subject = build(:subject)
+    expect(example.valid?).to eq(true)
+  end
 end
