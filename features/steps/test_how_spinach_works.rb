4e364432164cf9101bb88f50eabf525b7798ba92
Tomas Jukin <tomas.jukin@juicymo.cz>
2017-12-12 09:29:12 +0100

    Let spinach generate step definitions
    `RAILS_ENV=test rake spinach:generate`

---
+++ features/steps/test_how_spinach_works.rb
@@ -0,0 +1,25 @@
+class Spinach::Features::TestHowSpinachWorks < Spinach::FeatureSteps
+  step 'I have an empty array' do
+    pending 'step not implemented'
+  end
+
+  step 'I append my first name and my last name to it' do
+    pending 'step not implemented'
+  end
+
+  step 'I pass it to my super-duper method' do
+    pending 'step not implemented'
+  end
+
+  step 'the output should contain a formal greeting' do
+    pending 'step not implemented'
+  end
+
+  step 'I append only my first name to it' do
+    pending 'step not implemented'
+  end
+
+  step 'the output should contain a casual greeting' do
+    pending 'step not implemented'
+  end
+end
