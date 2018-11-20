61bebd244be440f53eda552c7513c6dbd34cfdb8
Tomas Jukin <tomas.jukin@juicymo.cz>
2018-11-20 13:39:15 +0100

    Create student dashboard index action
    Please NOTE how eager loading is applied

--- app/controllers/student_dashboard_controller.rb
+++ app/controllers/student_dashboard_controller.rb
@@ -1,4 +1,6 @@
 class StudentDashboardController < ApplicationController
   def index
+    @subjects = Subject.includes([:study_programs]).all
+    #@subjects = Subject.all # FUJKYYYY, here is N+1 problem!!!
   end
 end
